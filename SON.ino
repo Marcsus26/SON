6a279cc87bb4f27e25c66ed34ae886b5b871eb54
Marc Doumit <marc@Marcs-MacBook-Air.local>
2026-02-11 10:23:32 +0100

    On enregistre sur la carte SD

--- SON.ino
+++ SON.ino
@@ -1,50 +1,149 @@
 #include <Audio.h>
 #include <Bounce.h>
-#include "capture.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 
-capture capture;
 AudioInputI2S in;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
-AudioConnection patchCord1(in, 0, capture, 0);
-AudioConnection patchCord3(capture, 0, out, 0);
-AudioConnection patchCord4(capture, 0, out, 1);
+AudioAnalyzePeak peak1;
+AudioRecordQueue queue1;         
+AudioPlaySdRaw playRaw1;       
+AudioConnection patchCord1(in, 0, queue1, 0);
+AudioConnection patchCord2(in, 0, peak1, 0);
+AudioConnection patchCord3(playRaw1, 0, out, 0);
+AudioConnection patchCord4(playRaw1, 0, out, 1);
 
-
+#define SDCARD_CS_PIN    10
+#define SDCARD_MOSI_PIN  11   // Teensy 4 ignores this, uses pin 11
+#define SDCARD_SCK_PIN   13  // Teensy 4 ignores this, uses pin 13
 
 Bounce boutonCapture = Bounce(0, 10);
+int mode = 0;
+File frec;
 
 void setup() {
   Serial.begin(9600);
   pinMode(0, INPUT_PULLUP);
-  AudioMemory(6);
+  AudioMemory(60);
   audioShield.enable();
   audioShield.inputSelect(AUDIO_INPUT_MIC);
   audioShield.micGain(10); // in dB
   audioShield.volume(1);
-  capture.setParamValue("level (db)", 4.0);
+  SPI.setMOSI(SDCARD_MOSI_PIN);
+  SPI.setSCK(SDCARD_SCK_PIN);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here if no SD card, but print a message
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
 }
 
 void loop() {
   // A. On met à jour l'état du bouton physique
   boutonCapture.update();
 
-  // B. Si le bouton a changé d'état (Appuyé ou Relâché)
-  if (boutonCapture.risingEdge()) {
-    // Falling Edge = Passage de 3.3V à 0V = APPUI
-    // On envoie "1.0" au paramètre Faust
-    capture.setParamValue("Capture", 1.0);
-    Serial.println("Capture ON");
-  }
-  
+  // Respond to button presses
   if (boutonCapture.fallingEdge()) {
-    // Rising Edge = Passage de 0V à 3.3V = RELACHÉ
-    // On envoie "0.0" au paramètre Faust
-    capture.setParamValue("Capture", 0.0);
-    Serial.println("Capture OFF");
+    if (mode == 0) {
+      Serial.println("Started recording");
+      startRecording();
+    }
+    else { 
+      if (mode == 1) {
+        Serial.println("Stopped recording");
+        stopRecording();
+    }
+    else {
+      if (mode == 2) {
+        Serial.println("Started playing");
+        startPlaying();
+    }
+    }
+    }
   }
+  if (mode == 1) {
+    continueRecording();
+  }
+  if (mode == 3) {
+    continuePlaying();
+  }
+}
+
+void startRecording() {
+  if (SD.exists("RECORD.RAW")) {
+    // The SD library writes new data to the end of the
+    // file, so to start a new recording, the old file
+    // must be deleted before new data is written.
+    SD.remove("RECORD.RAW");
+  }
+  frec = SD.open("RECORD.RAW", FILE_WRITE);
+  if (frec) {
+    queue1.begin();
+    mode = 1;
+  }
+}
+
+void continueRecording() {
+  if (queue1.available() >= 2) {
+    byte buffer[512];
+    // Fetch 2 blocks from the audio library and copy
+    // into a 512 byte buffer.  The Arduino SD library
+    // is most efficient when full 512 byte sector size
+    // writes are used.
+    memcpy(buffer, queue1.readBuffer(), 256);
+    queue1.freeBuffer();
+    memcpy(buffer+256, queue1.readBuffer(), 256);
+    queue1.freeBuffer();
+    // write all 512 bytes to the SD card
+    //elapsedMicros usec = 0;
+    frec.write(buffer, 512);
+    // Uncomment these lines to see how long SD writes
+    // are taking.  A pair of audio blocks arrives every
+    // 5802 microseconds, so hopefully most of the writes
+    // take well under 5802 us.  Some will take more, as
+    // the SD library also must write to the FAT tables
+    // and the SD card controller manages media erase and
+    // wear leveling.  The queue1 object can buffer
+    // approximately 301700 us of audio, to allow time
+    // for occasional high SD card latency, as long as
+    // the average write time is under 5802 us.
+    //Serial.print("SD write, us=");
+    //Serial.println(usec);
+  }
+}
+
+void stopRecording() {
+  queue1.end();
+  if (mode == 1) {
+    while (queue1.available() > 0) {
+      frec.write((byte*)queue1.readBuffer(), 256);
+      queue1.freeBuffer();
+    }
+    frec.close();
+  }
+  mode = 2;
+}
+
+
+void startPlaying() {
+  playRaw1.play("RECORD.RAW");
+  mode = 3;
+}
+
+void continuePlaying() {
+  if (!playRaw1.isPlaying()) {
+    playRaw1.stop();
+    mode = 0;
+  }
+}
+
+void stopPlaying() {
+  Serial.println("stopPlaying");
+  if (mode == 2) playRaw1.stop();
+  mode = 0;
 }
